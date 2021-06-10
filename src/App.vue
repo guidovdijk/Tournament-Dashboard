16d14a278ed4f6f2406c822b679b2dacd3e3e9ad
Guido van Dijk <funnydutchguy@gmail.com>
2021-06-10 21:51:13 +0200

    Added background styling

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Background />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Background from './components/Background.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Background,
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Background.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="background">
+    <div class="background__color-gradient background__color-gradient--green"></div>
+    <div class="background__color-gradient background__color-gradient--black"></div>
+    <div class="background__color-blend"></div>
+    <div class="background__image"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Background"
+}
+</script>
