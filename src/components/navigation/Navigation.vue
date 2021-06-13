e1c3bedfb318b2e0bcc505fed2e217ba076be7bb
Guido van Dijk <funnydutchguy@gmail.com>
2021-06-13 19:01:48 +0200

    added match history component + styling changes

--- src/components/navigation/Navigation.vue
+++ src/components/navigation/Navigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-navbar>
+  <b-navbar fixed-top>
       <template #brand>
           <b-navbar-item tag="router-link" :to="{ path: '/' }">
               <img
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="columns">
+  <div class="columns is-multiline">
     <div class="column is-3 is-offset-1">
       <PlayerProfile :player="player"/>
     </div>
@@ -10,7 +10,44 @@
         <StatsBox title="Most Wins With" value="Ipsum Mana" icon="account"/>
         <StatsBox title="Points Earned" value="158" icon="currency-usd"/>
       </div>
+    </div>
 
+    <div class="column is-8">
+      <div class="match-history-wrapper">
+        <h3 class="title is-5">Match History</h3>
+
+        <b-table :data="data">
+          <b-table-column field="type" label="Type" v-slot="props">
+            {{
+              props.row.type
+            }}
+          </b-table-column>
+          <b-table-column field="team" label="Team" v-slot="props">
+            {{
+              props.row.team
+            }}
+          </b-table-column>
+          <b-table-column field="placement" label="Placement" v-slot="props">
+            {{
+              props.row.placement
+            }}
+            /
+            {{
+              props.row.allTeams
+            }}
+          </b-table-column>
+          <b-table-column field="points" label="Points" v-slot="props">
+            <span class="has-text-primary">
+              {{
+                props.row.points
+              }}
+            </span>
+          </b-table-column>
+          <b-table-column  field="id" label="" v-slot="props">
+            <router-link :to="'/tournament/'+props.row.id" class="is-link">Details</router-link>
+          </b-table-column>
+        </b-table>
+      </div>
     </div>
   </div>
 </template>
@@ -32,7 +69,15 @@ export default {
         level: 21,
         currentExp: 52,
         totalExp: 104
-      }
+      },
+      data: [
+        { 'id': 1, 'type': "ARAM", 'team': 'Lorem Mana', 'placement': 3, 'allTeams': 10, 'points': '+31', 'details': '' },
+        { 'id': 2, 'type': "ARAM", 'team': 'Lorem Mana', 'placement': 3, 'allTeams': 10, 'points': '+31', 'details': '' },
+        { 'id': 3, 'type': "ARAM", 'team': 'Lorem Mana', 'placement': 3, 'allTeams': 10, 'points': '+31', 'details': '' },
+        { 'id': 4, 'type': "ARAM", 'team': 'Lorem Mana', 'placement': 3, 'allTeams': 10, 'points': '+31', 'details': '' },
+        { 'id': 4, 'type': "ARAM", 'team': 'Lorem Mana', 'placement': 3, 'allTeams': 10, 'points': '+31', 'details': '' },
+        { 'id': 4, 'type': "ARAM", 'team': 'Lorem Mana', 'placement': 3, 'allTeams': 10, 'points': '+31', 'details': '' },
+      ],
     }
   }
 }
