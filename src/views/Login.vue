a806fd7b9e370762ef8220bd22961511a8fc26c7
Guido van Dijk <funnydutchguy@gmail.com>
2021-06-16 15:35:42 +0200

    Removed form components

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,54 @@
 <template>
 <div class="columns is-centered">
   <div class="column is-5 is-4-desktop">
-    <LoginForm />
+    <form class="box box--login" @submit.prevent="login">
+      <h3 class="title is-4">Login</h3>
+      <b-field label="Email"
+          message="">
+          <b-input
+            type="email"
+            v-model="loginFormData.email"
+            placeholder="dr.mundoe@gmail.com...">
+          </b-input>
+      </b-field>
+      <b-field label="Password">
+        <b-input type="password"
+            placeholder="cle(a)ver-password..."
+            v-model="loginFormData.password"
+            password-reveal>
+        </b-input>
+      </b-field>
+      <b-field grouped class="is-centered">
+        <b-button tag="input"
+          native-type="submit"
+          value="Login"
+          type="is-primary"
+        />
+        <p class="control">
+          <span>or</span>
+          <router-link to="/register" class="is-link">Sign up</router-link>
+        </p>
+      </b-field>
+    </form>
   </div>
 </div>
 </template>
 
 <script>
-import LoginForm from "../components/forms/Login.vue";
 export default {
   name: "Login",
-  components: {
-    LoginForm
+  data(){
+    return {
+      loginFormData: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    login: function(){
+      console.log(this.loginFormData);
+    }
   }
 }
 </script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,17 +1,75 @@
 <template>
 <div class="columns is-centered">
   <div class="column is-5 is-4-desktop">
-    <RegisterForm />
+    <form class="box box--register" @submit.prevent="register">
+      <h3 class="title is-4">Register</h3>
+      <b-field label="Email"
+          message="">
+          <b-input
+            type="email"
+            required
+            v-model="registerFormData.email"
+            placeholder="dr.mundoe@gmail.com...">
+          </b-input>
+      </b-field>
+      <b-field label="Discord Name"
+          message="">
+          <b-input
+            type="text"
+            required
+            v-model="registerFormData.username"
+            placeholder="dr.mundoe...">
+          </b-input>
+      </b-field>
+      <b-field label="Password">
+        <b-input type="password"
+            required
+            v-model="registerFormData.password"
+            placeholder="cle(a)ver-password..."
+            password-reveal>
+        </b-input>
+      </b-field>
+      <b-field label="Re-Type Password">
+        <b-input type="password"
+            required
+            v-model="registerFormData.retypePassword"
+            placeholder="cle(a)ver-password..."
+            password-reveal>
+        </b-input>
+      </b-field>
+      <b-field grouped class="is-centered">
+        <b-button tag="input"
+          native-type="submit"
+          value="Register"
+          type="is-primary"
+        />
+        <p class="control">
+          <span>or</span>
+          <router-link to="/login" class="is-link">Go back to login</router-link>
+        </p>
+      </b-field>
+    </form>
   </div>
 </div>
 </template>
 
 <script>
-import RegisterForm from "../components/forms/Register.vue";
 export default {
   name: "Register",
-  components: {
-    RegisterForm
+  data(){
+    return {
+      registerFormData: {
+        email: '',
+        username: '',
+        password: '',
+        retypePassword: ''
+      }
+    }
+  },
+  methods: {
+    register: function(){
+      console.log(this.registerFormData);
+    }
   }
 }
 </script>
